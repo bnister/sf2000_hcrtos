62d9b6103468a866410ae3790d7b77f8c3a4d51f
Nikita Burnashev <nikita.burnashev@gmail.com>
2023-06-17 23:17:18 +0300

    Refactor crc.c

--- buildSf.sh
+++ buildSf.sh
@@ -2,7 +2,5 @@ make O=bl sf2000_min_bl_defconfig
 make O=bl all
 make sf2000_min_defconfig
 make all
-dd if=/dev/zero bs=4096 count=1 | cat - output/images/avp.bin > output/images/bisrv.asd
+gcc -o crc crc.c && ./crc output/images/avp.bin output/images/bisrv.asd && rm crc
 cp output/images/bisrv.asd .
-echo LCFG | dd of=bisrv.asd bs=4 conv=notrunc
-cat crc.c | gcc -xc - && ./a.out && rm a.out
--- crc.c
+++ crc.c
@@ -1,30 +1,81 @@
+/* Copyright (C) 2023 Nikita Burnashev
+
+Permission to use, copy, modify, and/or distribute this software
+for any purpose with or without fee is hereby granted.
+
+THIS SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND! */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdint.h>
 
-int main(void) {
+#define IMAGE_START 0x1000
+#define LCFG_HEADER_SIZE 0x200
+
+int main(int argc, char *argv[])
+{
 	uint32_t tab[256], c;
 	int i, j;
-	FILE *pf = fopen("bisrv.asd", "rb+");
-	static uint8_t buf[8192];
+	FILE *pf;
+	uint8_t *buf;
 	size_t sz;
 
+	if (argc < 3) {
+		fprintf(stderr, "Usage: %s <output/images/avp.bin> <output/images/bisrv.asd>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	pf = fopen(argv[1], "rb");
+	if (pf == NULL) {
+		fprintf(stderr, "Cannot open %s for reading\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	fseek(pf, 0, SEEK_END);
+	sz = ftell(pf);
+	buf = malloc(IMAGE_START + sz);
+	fseek(pf, 0, SEEK_SET);
+	fread(&buf[IMAGE_START], 1, sz, pf);
+	fclose(pf);
+
+	memset(buf, 0, IMAGE_START); /* MIPS NOPs */
+	sz = IMAGE_START + sz - LCFG_HEADER_SIZE;
+
+	/* CRC-32/MPEG-2 variant https://reveng.sourceforge.io/crc-catalogue/all.htm#crc.cat.crc-32-mpeg-2 */
 	for (i = 0; i < 256; i++) {
 		c = (unsigned long)i << 24;
-		for (j = 0; j < 8; j++)
+		for (j = 0; j < 8; j++) {
 			c = c & (1 << 31) ? c << 1 ^ 0x4c11db7 : c << 1;
+		}
 		tab[i] = c;
 	}
 	c = 0xffffffff;
-	fseek(pf, 512, SEEK_SET);
-	while ((sz = fread(buf, 1, sizeof buf, pf)) > 0) {
-		for (i = 0; i < sz; i++) c = c << 8 ^ tab[c >> 24 ^ buf[i]];
+	for (i = LCFG_HEADER_SIZE; i < LCFG_HEADER_SIZE + sz; i++) {
+		c = c << 8 ^ tab[c >> 24 ^ buf[i]];
 	}
-	buf[0] = c & 255;
-	buf[1] = c >> 8 & 255;
-	buf[2] = c >> 16 & 255;
-	buf[3] = c >> 24;
-	fseek(pf, 0x18c, SEEK_SET);
-	fwrite(buf, 1, 4, pf);
+
+	buf[0] = 'L';
+	buf[1] = 'C';
+	buf[2] = 'F';
+	buf[3] = 'G';
+
+	buf[0x184] = sz & 255;
+	buf[0x185] = sz >> 8 & 255;
+	buf[0x186] = sz >> 16 & 255;
+	buf[0x187] = sz >> 24;
+
+	buf[0x18c] = c & 255;
+	buf[0x18d] = c >> 8 & 255;
+	buf[0x18e] = c >> 16 & 255;
+	buf[0x18f] = c >> 24;
+
+	pf = fopen(argv[2], "wb");
+	if (pf == NULL) {
+		fprintf(stderr, "Cannot open %s for writing\n", argv[2]);
+		return EXIT_FAILURE;
+	}
+	fwrite(buf, 1, LCFG_HEADER_SIZE + sz, pf);
 	fclose(pf);
-	return 0;
+
+	return EXIT_SUCCESS;
 }
