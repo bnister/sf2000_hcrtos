4a37392571123e12ffe7d3cef1d4fadcabf9e49e
Ignatz <ignatz@maschath.de>
2023-06-18 19:42:22 +0200

    Add rsync to ci and change toolchain order

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,3 +1,3 @@
 apt update
-apt install wget git gcc flex bison gperf make python unzip rar dos2unix swig python-dev python3-dev python3-pip clang-format python3 python3-pip python3-setuptools python3-wheel ninja-build rename gdb apache2 re2c ctags lzip libncurses-dev tree pkg-config cmake python-pip automake lzop doxygen graphviz libssl-dev genromfs lzma lzma mtools mtd-tools -y
+apt install wget git gcc flex bison gperf make python unzip rar dos2unix swig python-dev python3-dev python3-pip clang-format python3 python3-pip python3-setuptools python3-wheel ninja-build rename gdb apache2 re2c ctags lzip libncurses-dev tree pkg-config cmake python-pip automake lzop doxygen graphviz libssl-dev genromfs lzma lzma mtools mtd-tools rsync -y
 pip3 install fdt
--- install-toolchain.sh
+++ install-toolchain.sh
@@ -1,5 +1,5 @@
 wget https://cloud.maschath.de/s/dDrZN4DSgJ33E3w/download/Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
 wget https://cloud.maschath.de/s/aLtJ86KbXmTm6iE/download/Codescape.GNU.Tools.Package.2019.09-03.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz
-tar -xzf Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
 tar -xzf Codescape.GNU.Tools.Package.2019.09-03.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
+tar -xzf Codescape.GNU.Tools.Package.2019.09-03-2.for.MIPS32.MTI.Bare.Metal.Ubuntu-18.04.5.x86_64.tar.gz -C /opt
 
