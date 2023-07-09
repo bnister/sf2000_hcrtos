ebe5c35af0c749e690e407240175ea66940d80ea
Ignatz <ignatz@maschath.de>
2023-07-09 12:08:15 +0200

    some file cleanup

--- components/applications/apps-sf2000/source/sf2000_app/sf2000.rtos.mk
+++
@@ -1,3 +0,0 @@
-#CSRCS += $(shell find -L $(PROJECTOR_DIR)/sf2000_app -name "*.c")
-CSRCS += $(PROJECTOR_DIR)/sf2000_app/main_sf2000_retroarch.c
-CFLAGS += -I$(PROJECTOR_DIR)
--- components/applications/apps-sf2000/source/sf2000_app/sf2000_app.mk
+++
@@ -1,3 +0,0 @@
-SF2000_SRC += $(shell find -L $(LVGL_DIR)/sf2000_app -name "*.c")
-SF2000_CPP_SRC += $(shell find -L $(LVGL_DIR)/sf2000_app -name "*.cpp")
-
